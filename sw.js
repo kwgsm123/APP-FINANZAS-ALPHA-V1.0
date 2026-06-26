33e37e8f3b6c3c3cc3137d7d2d50e552649d0991
kwgsm123 <karlwilliam.gonzalez@gmail.com>
2026-06-25 23:33:18 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,14 @@
-const CACHE = 'mis-finanzas-v2';
-const ARCHIVOS = ['./', './index.html', './style.css', './app.js', './manifest.json'];
+const CACHE = 'mis-finanzas-v3';
+const BASE = '/APP-FINANZAS-ALPHA-V1.0/';
+const ARCHIVOS = [
+  BASE,
+  BASE + 'index.html',
+  BASE + 'style.css',
+  BASE + 'app.js',
+  BASE + 'manifest.json',
+  BASE + 'icons/icon-192.png',
+  BASE + 'icons/icon-512.png'
+];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ARCHIVOS)));
